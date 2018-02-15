add800ab7efb88206eaa2e255622da54982783ed
markliu152 <1099134352@qq.com>
2018-02-15 12:44:51 +0800

    first commit

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['zabbix']['agent']['server'] = '192.168.0.188'
+default['zabbix']['agent']['hostname'] = 'fqdn'
+default['port'] = '10050/tcp'
+default['zabbix']['agent']['timeout'] = '30'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,20 @@
 # Cookbook:: zabbix-agent
 # Recipe:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# Copyright:: 2018, Mark Liu, All Rights Reserved.
+
+include_recipe 'zabbix1::release'
+include_recipe 'zabbix1::firewall'
+
+package 'zabbix-agent'do
+
+template '/etc/zabbix/zabbix_agentd.conf' do
+  source 'agent.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+service 'zabbix-proxy' do
+  action [:enable, :start]
+end
